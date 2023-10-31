ee1f8d01894870fccb5f6d362ba78807169d42da
vancauwe <laure.vancauwenberghe@epfl.ch>
2023-10-31 11:06:59 +0000

    updated consolidate metadata flow

--- zarr_linked_data/consolidate_metadata_flow.py
+++ zarr_linked_data/consolidate_metadata_flow.py
@@ -1,5 +1,4 @@
-import zarr
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, step, current, Parameter
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,18 +7,20 @@ load_dotenv()
 
 class MetadataConsolidateFlow(FlowSpec):
 
-    # path_store = Parameter(
-    #     "path_store",
-    #     help="The path to the Zarr Store who's metadata we want to consolidate into a Zarr MetadataStore.",
-    # )
+    path_for_store = Parameter(
+        "path_for_store",
+        help="The path to the Zarr Store who's metadata we want to consolidate into a Zarr MetadataStore.",
+    )
 
     @step
     def start(self):
         """Start the consolidation flow."""
         import zarr
-
+        #path_for_store = "zarr_linked_data/data/test_store.zarr"
         zarr.convenience.consolidate_metadata(
-            store="../data/test_store.zarr", metadata_key=".all_metadata"
+            #store=path_for_store, 
+            store=self.path_for_store,
+            metadata_key=".all_metadata"
         )
         self.next(self.end)
 
@@ -34,11 +35,13 @@ if __name__ == "__main__":
     ###### CONSOLIDATE METADATA FLOW
     # ----------------------------------------------
     # BIG ISSUE: how to automate this if not compatible with metaflow ?
-    path_for_store = "zarr_linked_data/data/test_store.zarr"
-    zarr.convenience.consolidate_metadata(
-        path_for_store, metadata_key=".all_metadata"
-    )
+    # path_for_store = "zarr_linked_data/data/test_store.zarr"
+    # zarr.convenience.consolidate_metadata(
+    #     path_for_store, metadata_key=".all_metadata"
+    # )
 
     # The consolidation flow is not functional:
     # it renders no errors but no metadata store is created.
-    #MetadataConsolidateFlow()
+    # call with:
+    # python zarr_linked_data/consolidate_metadata_flow.py run --path_for_store="zarr_linked_data/data/test_store.zarr"
+    MetadataConsolidateFlow()
